f8d81f53e3347b4c7ee94ed7406d17c5f499f9ff
Roee-M <roeemarom@campus.technion.ac.il>
2022-05-22 12:10:36 +0000

    copilation
    try to compile, ther are problems in the hendler

--- part2/ili_handler.asm
+++ part2/ili_handler.asm
@@ -3,98 +3,100 @@
 .text
 .align 4, 0x90
 my_ili_handler:
-    # Prolog #
-    pushq %rbp
+	# Prolog #
+	pushq %rbp
 	movq %rsp, %rbp
-    movq (%rbp), %rdi # keep rip, that saved on stack, in rdi.
+	phshq %rdi # ????? add    
+	movq (%rbp), %rdi # keep rip, that saved on stack, in rdi.
 
-    pushq %r8
-    # mov 8(%rsp), %r8 # keep rip, that saved on stack, in r8.
-    pushq %r9
-    pushq %r10
-    pushq %r11
-    pushq %r12
-    pushq %r13
-    pushq %r14
-    pushq %r15
-    pushq %rax
-    pushq %rbx
-    pushq %rcx
-    pushq %rdx
-    pushq %rbp
-    phshq %rdi # ????? add
-    pushq %rsi
-    pushq %rsp
+	pushq %r8
+	# mov 8(%rsp), %r8 # keep rip, that saved on stack, in r8.
+	pushq %r9
+	pushq %r10
+	pushq %r11
+	pushq %r12
+	pushq %r13
+	pushq %r14
+	pushq %r15
+	pushq %rax
+	pushq %rbx
+	pushq %rcx
+	pushq %rdx
+	pushq %rbp
+	pushq %rsi
+	pushq %rsp
 
-    movq (%rdi), %rbx # rbx is the opcode low bit
-    xorq %rdi, %rdi # Prepares the RDI to be the input of "what_to_do"
-    xorq %rdx, %rdx
+	movq (%rdi), %rbx # rbx is the opcode low bit
+	xorq %rdi, %rdi # Prepares the RDI to be the input of "what_to_do"
+	xorq %rdx, %rdx
 
-    cmpb 0x0f, %bl
-    je opcode_is_two_bytes
+	cmpb 0x0f, %bl
+	je opcode_is_two_bytes
 
-    opcode_is_one_byte:
-    movb %bl, %dil # ????? was %bl, %dl
-    # ??? movq %rdx, %rdi # Prepares the RDI to be the input of "what_to_do"
-    jmp call_what_to_do
+	opcode_is_one_byte:
+	movb %bl, %dil # ????? was %bl, %dl
+	# ??? movq %rdx, %rdi # Prepares the RDI to be the input of "what_to_do"
+	jmp call_what_to_do
 
-    opcode_is_two_bytes:
-    movb %bh, %dil # ???? movb %bh, %dl
-    movq %rdx, %rdi # Prepares the RDI to be the input of "what_to_do"
-    
-    call_what_to_do:
-    call what_to_do
-    cmpq $0, %rax
-    jne our_treatment_routine
+	opcode_is_two_bytes:
+	movb %bh, %dil # ???? movb %bh, %dl
+	movq %rdx, %rdi # Prepares the RDI to be the input of "what_to_do"
 
-    original_treatment_routine:
-    # Epilog #
+	call_what_to_do:
+	call what_to_do
+	cmpq $0, %rax
+	jne our_treatment_routine
+
+	original_treatment_routine:
+	# Epilog #
 	popq %rsp
-    popq %rsi
-    popq %rdi
-    popq %rbp
-    popq %rdx
-    popq %rcx
-    popq %rbx
-    popq %rax
-    popq %r15
-    popq %r14
-    popq %r13
-    popq %r12
-    popq %r11
-    popq %r10
-    popq %r9
-    popq %r8
-    popq %rbp
+	popq %rsi
+	popq %rdi
+	popq %rbp
+	popq %rdx
+	popq %rcx
+	popq %rbx
+	popq %rax
+	popq %r15
+	popq %r14
+	popq %r13
+	popq %r12
+	popq %r11
+	popq %r10
+	popq %r9
+	popq %r8
+	popq %rdi
+	popq %rbp
 
-    jmp * old_ili_handler
-    jmp end_HW2
+	jmp * old_ili_handler
+	jmp end_HW2
 
-    our_treatment_routine:
-    movq %rax, %rdi
+	our_treatment_routine:
+	movq %rax, %rdi
 
-    # Epilog #
+	# Epilog #
 	popq %rsp
-    popq %rsi
-    popq %rdi
-    popq %rbp
-    popq %rdx
-    popq %rcx
-    popq %rbx
-    movq %rax, %rdi
+	popq %rsi
+	popq %rdi
+	popq %rbp
+	popq %rdx
+	popq %rcx
+	popq %rbx
+	movq %rax, %rdi
 
-    popq %rax
-    popq %r15
-    popq %r14
-    popq %r13
-    popq %r12
-    popq %r11
-    popq %r10
-    popq %r9
-    popq %r8
-    popq %rbp
-    
-    addq $2, (%rsp)
+	popq %rax
+	popq %r15
+	popq %r14
+	popq %r13
+	popq %r12
+	popq %r11
+	popq %r10
+	popq %r9
+	popq %r8
+	popq
+	popq %rbp
 
-    end_HW2:
-    iretq
+	addq $2, (%rsp)
+
+	end_HW2:
+	iretq
